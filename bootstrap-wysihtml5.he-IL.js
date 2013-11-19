de53769b70cdec7995843e4fe8dc860e22a12209
eladcm1 <elad@iname.com>
2013-11-19 15:36:07 +0200

    Create bootstrap-wysihtml5.he-IL.js
    hebrew local file

---
+++ bootstrap-wysihtml5.he-IL.js
@@ -0,0 +1,49 @@
+/**
+ * Hebrew translation for bootstrap-wysihtml5
+ */
+(function($){
+    $.fn.wysihtml5.locale["he-IL"] = {
+        font_styles: {
+              normal: "טקסט רגיל",
+              h1: "כותרת 1",
+              h2: "כותרת 2",
+              h3: "כותרת 3"
+        },
+        emphasis: {
+              bold: "מודגש",
+              italic: "טקסט נטוי",
+              underline: "קו תחתון"
+        },
+        lists: {
+              unordered: "רשימה עם תבליטים",
+              ordered: "רשימה ממוספרת",
+              outdent: "הקטן כניסה",
+              indent: "הגדל כניסה",
+              indered: "הגדל כניסה"
+        },
+        link: {
+              insert: "הכנס קישור",
+              cancel: "בטל קישור"
+        },
+        image: {
+              insert: "הוסף תמונה",
+              cancel: "בטל"
+        },
+        html: {
+            edit: "עורך HTML"
+        },
+        colours: {
+          black: "שחור",
+          silver: "כסף",
+          gray: "אפור",
+          maroon: "חום",
+          red: "אדום",
+          purple: "סגול",
+          green: "ירוק",
+          olive: "ירוק זית",
+          navy: "כחול צי",
+          blue: "כחול",
+          orange: "כתום"
+        }
+    };
+}(jQuery));
